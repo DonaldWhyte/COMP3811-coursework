183f37490600992d190740cdbcff794cae1de705
Donald Whyte <sc10dw@cslin041.csunix.comp.leeds.ac.uk>
2013-10-05 18:59:51 +0100

    Added skeleton of a 3D vector class

---
+++ Vector3.h
@@ -0,0 +1,19 @@
+#ifndef MATH_VECTOR_H
+#define MATH_VECTOR_H
+
+class Vector3
+{
+
+public:
+	Vector3();
+	Vector3(float x, float y, float z);
+
+	float x;
+	float y;
+	float z;
+
+};
+
+
+
+#endif 
