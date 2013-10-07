d437567d5fefa6cfbcceee6cbb8587f1474ab6ca
Donald Whyte <sc10dw@cslin078.csunix.comp.leeds.ac.uk>
2013-10-07 17:45:23 +0100

    Removed unused code

--- GLCanvasWidget.cpp
+++ GLCanvasWidget.cpp
@@ -57,13 +57,7 @@ void GLCanvasWidget::paintGL()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Setup a look-at matrix for a camera perspective
-	glPushMatrix();
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-		drawable->render();
-
-	glPopMatrix();
+	drawable->render();
 
 	glFlush();
 }
