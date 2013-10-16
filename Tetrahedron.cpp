f8c2a901501285487033f885b4bfab3a8b5861cf
Donald Whyte <sc10dw@cslin018.csunix.comp.leeds.ac.uk>
2013-10-16 10:43:34 +0100

    Added triangles to Tetrahedron

--- Tetrahedron.cpp
+++ Tetrahedron.cpp
@@ -9,6 +9,10 @@ Tetrahedron::Tetrahedron()
 	vertices.push_back( Vector3(1.0f, 1.0f, -1.0f) );
 	lines.push_back( Line(0, 1) ); lines.push_back( Line(1, 2) ); lines.push_back( Line(2, 0) ); // bottom lines
 	lines.push_back( Line(0, 3) ); lines.push_back( Line(1, 3) ); lines.push_back( Line(2, 3) ); // lines going to tip
+	triangles.push_back( Triangle(0, 1, 2) );
+	triangles.push_back( Triangle(3, 0, 1) );
+	triangles.push_back( Triangle(3, 1, 2) );
+	triangles.push_back( Triangle(3, 2, 0) );
 }
 
 const Vector3List& Tetrahedron::getVertices() const
