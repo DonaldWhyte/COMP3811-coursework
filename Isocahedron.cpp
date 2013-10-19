fc5f5532914d4e249ce91bf1a2364fe658aa3bda
Donald Whyte <sc10dw@cslin040.csunix.comp.leeds.ac.uk>
2013-10-19 14:18:39 +0100

    Added line indicies for isocahedron

--- Isocahedron.cpp
+++ Isocahedron.cpp
@@ -1,6 +1,8 @@
 #include <math.h>
 #include "Isocahedron.h"
 
+#include <fstream>
+
 // Pre-computed for performance
 static const float PHI = 1.25663706144; 
 
@@ -24,6 +26,55 @@ Isocahedron::Isocahedron()
 	vertices.push_back( Vector3(0.707106f, 0.0f, 0.707106f) );
 	vertices.push_back( Vector3(0.707106f, 0.707106f, 0.0f) );
 	vertices.push_back( Vector3(1.0f, 0.0f, 0.0f) );
+
+	lines.push_back( Line(11, 16) );
+	lines.push_back( Line(14, 17) );
+	lines.push_back( Line(1, 3) );
+	lines.push_back( Line(14, 16) );
+	lines.push_back( Line(5, 6) );
+	lines.push_back( Line(2, 8) );
+	lines.push_back( Line(3, 11) );
+	lines.push_back( Line(10, 12) );
+	lines.push_back( Line(13, 17) );
+	lines.push_back( Line(4, 12) );
+	lines.push_back( Line(1, 6) );
+	lines.push_back( Line(16, 17) );
+	lines.push_back( Line(12, 16) );
+	lines.push_back( Line(3, 7) );
+	lines.push_back( Line(0, 3) );
+	lines.push_back( Line(6, 14) );
+	lines.push_back( Line(1, 2) );
+	lines.push_back( Line(8, 14) );
+	lines.push_back( Line(5, 13) );
+	lines.push_back( Line(7, 15) );
+	lines.push_back( Line(9, 15) );
+	lines.push_back( Line(8, 10) );
+	lines.push_back( Line(1, 5) );
+	lines.push_back( Line(9, 11) );
+	lines.push_back( Line(0, 4) );
+	lines.push_back( Line(5, 7) );
+	lines.push_back( Line(15, 17) );
+	lines.push_back( Line(4, 10) );
+	lines.push_back( Line(2, 6) );
+	lines.push_back( Line(13, 15) );
+	lines.push_back( Line(2, 10) );
+	lines.push_back( Line(3, 9) );
+	lines.push_back( Line(11, 12) );
+	lines.push_back( Line(4, 11) );
+	lines.push_back( Line(7, 9) );
+	lines.push_back( Line(0, 1) );
+	lines.push_back( Line(6, 8) );
+	lines.push_back( Line(13, 14) );
+	lines.push_back( Line(10, 16) );
+	lines.push_back( Line(7, 13) );
+	lines.push_back( Line(2, 4) );
+	lines.push_back( Line(10, 14) );
+	lines.push_back( Line(6, 13) );
+	lines.push_back( Line(1, 7) );
+	lines.push_back( Line(11, 15) );
+	lines.push_back( Line(15, 16) );
+	lines.push_back( Line(3, 4) );
+	lines.push_back( Line(0, 2) );
 
 	triangles.push_back( Triangle(0, 2, 1) );
 	triangles.push_back( Triangle(0, 4, 2) );
