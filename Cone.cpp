ad3c1804d1d431cd3d8790c0e4c3a2ad3d0019e6
Donald Whyte <sc10dw@cslin040.csunix.comp.leeds.ac.uk>
2013-10-26 13:47:44 +0100

    Created better cone normals and fixed surface normal generation (it was REVERSING the direction of the normal)

--- Cone.cpp
+++ Cone.cpp
@@ -7,15 +7,19 @@ Cone::Cone(float height, float radius, int numSegments) : Mesh()
 	VertexList generatedVertices;
 	TriangleList generatedTriangles;
 	int vertexCounter = 0;
-	// Generate top vertex
+	// Generate top and bottom vertices
 	float halfHeight  (height / 2.0f);
 	Vertex topCentre;
 	topCentre.position = Vector3(0.0f, 0.0f, halfHeight);
 	topCentre.normal = topCentre.position.normalise();
 	topCentre.texCoord = TexCoord(1.0f, 0.5f);
 	generatedVertices.push_back(topCentre);
-	vertexCounter++;
-
+	Vertex bottomCentre;
+	bottomCentre.position = Vector3(0.0f, 0.0f, -halfHeight);
+	bottomCentre.normal = bottomCentre.position.normalise();
+	bottomCentre.texCoord = TexCoord(0.5, 0.5f);
+	generatedVertices.push_back(bottomCentre);
+	vertexCounter += 2;
 	// Generate the 'tower' part of the cone
 	for (float i = 0.0f; (i < numSegments); i += 1.0f)
 	{
@@ -23,39 +27,29 @@ Cone::Cone(float height, float radius, int numSegments) : Mesh()
 		float nextAngle = (PI * (i + 1.0f) * 2.0f) / numSegments;
 		float x1 = radius * sin(angle), y1 = radius * cos(angle);
 		float x2 = radius * sin(nextAngle), y2 = radius * cos(nextAngle);
-		
-		Vertex v1, v2;
+		// We compute these two vertices TWICE. This is because we want
+		// different normals at the bottom of the cone
+		Vertex v1, v2, bv1, bv2;
 		v1.position = Vector3(x1, y1, -halfHeight);
-		v1.normal = v1.position.normalise();
-		v1.texCoord = computeTexCoord(angle, v1.position.z, height);
 		v2.position = Vector3(x2, y2, -halfHeight);
-		v2.normal = v2.position.normalise();
+		v1.texCoord = computeTexCoord(angle, v1.position.z, height);
 		v2.texCoord = computeTexCoord(nextAngle, v2.position.z, height);
+		bv1 = v1;
+		bv2 = v2;
+		v1.normal = v1.position.normalise();
+		v2.normal = v2.position.normalise();
+		bv1.normal = bv2.normal = Vector3(0.0f, 0.0f, -1.0f);
 		generatedVertices.push_back(v1);
 		generatedVertices.push_back(v2);
+		generatedVertices.push_back(bv1);
+		generatedVertices.push_back(bv2);
 	
+		// Triangle for tower part
 		generatedTriangles.push_back( Triangle(0, vertexCounter, vertexCounter + 1) );
-		vertexCounter += 2;
+		// Triangle for bottom section
+		generatedTriangles.push_back( Triangle(1, vertexCounter + 3, vertexCounter + 2) );
+		vertexCounter += 4;
 	}
-	// Generate top and bottom sections
-	Vertex bottomCentre;
-	bottomCentre.position = Vector3(0.0f, 0.0f, -halfHeight);
-	bottomCentre.normal = bottomCentre.position.normalise();
-	bottomCentre.texCoord = TexCoord(0.5, 0.5f);
-	generatedVertices.push_back(bottomCentre);
-	vertexCounter++;
-	// each PAIR of vertices and the central vertex is used for triangle
-	for (unsigned int i = 0; (i < numSegments - 1); i++)
-	{
-		int index1 = (i * 2) + 1;
-		int index2 = vertexCounter - 1;
-		int index3 = (i * 2) + 2;
-		Triangle bottomTri(index1, index2, index3);
-		generatedTriangles.push_back(bottomTri);
-	}
-	// last triangle to fill bottom section
-	generatedTriangles.push_back( Triangle(((numSegments -1) * 2) + 1, vertexCounter - 1, 1) );
-	vertexCounter++;
 
 	setVertices(generatedVertices);
 	setTriangles(generatedTriangles);
