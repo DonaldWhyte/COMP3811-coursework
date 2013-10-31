fe003d99362d496110a0216a38b7ffa2445529b7
Donald Whyte <sc10dw@cslin071.csunix.comp.leeds.ac.uk>
2013-10-31 13:17:49 +0000

    Created skeleton for standard set of axes

--- GLApplicationMain.cpp
+++ GLApplicationMain.cpp
@@ -12,11 +12,15 @@ int main(int argc, char* argv[])
 
 	// Construct skeleton to draw (model)
 	SurfaceFactory surfaceFactory;
-	Surface cylinder = surfaceFactory.createCone(0.75f, 0.25f, 8);
-	Bone rootBone(&cylinder);
+	Surface cylinder = surfaceFactory.createCylinder(7.5f, 0.1f, 32);
+	Surface cone = surfaceFactory.createCone(0.75f, 0.1f, 32);	
+	Bone rootBone(NULL);
+	rootBone.addChild( new Bone(&cylinder, Vector3(0, 0, 0), Vector3(0, 0, 0)) );
+	rootBone.addChild( new Bone(&cylinder, Vector3(0, 0, 0), Vector3(0, 90, 0)) );
+	rootBone.addChild( new Bone(&cylinder, Vector3(0, 0, 0), Vector3(90, 0, 0)) );
 
 	Drawable* drawable = new Skeleton(&rootBone);
-	drawable->setZ(-2.5f); // so drawable is visible
+	drawable->setZ(-10.0f); // so drawable is visible
 
 	// Now build view and controller
 	GLWindow* window = new GLWindow(NULL, drawable);
--- surfaces/Surface.cpp
+++ surfaces/Surface.cpp
@@ -1,6 +1,36 @@
 #include "Surface.h"
 #include <QGLWidget>
 
+// Functions only used in this translation unit - don't need to be in header!
+void renderPoints(const VertexList& vertices)
+{
+	glBegin(GL_POINTS);
+	for (VertexList::const_iterator v = vertices.begin(); (v != vertices.end()); v++)
+		glVertex3fv(v->position.elems);
+	glEnd();
+
+}
+void renderLines(const VertexList& vertices, const TriangleList& triangles)
+{
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glBegin(GL_LINES);
+                for (TriangleList::const_iterator it = triangles.begin(); (it != triangles.end()); it++)
+                {
+                        const Vertex& v1 = vertices[it->v1];
+                        const Vertex& v2 = vertices[it->v2];
+                        const Vertex& v3 = vertices[it->v3];
+                        glVertex3fv(v1.position.elems);
+                        glVertex3fv(v2.position.elems);
+                        glVertex3fv(v2.position.elems);
+                        glVertex3fv(v3.position.elems);
+                        glVertex3fv(v3.position.elems);                                                                                                
+                        glVertex3fv(v1.position.elems);                                                                                                                     
+                }
+        glEnd();
+}
+
+
+
 Surface::Surface(const VertexList& vertices, const TriangleList& triangles) :
 	verts(vertices), tris(triangles)
 {
@@ -26,42 +56,13 @@ void Surface::setTriangles(const TriangleList& newTriangles)
 	tris = newTriangles;
 }
 
-
-void renderLines(const VertexList& vertices, const TriangleList& triangles)
-{
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glBegin(GL_LINES);
-                for (TriangleList::const_iterator it = triangles.begin(); (it != triangles.end()); it++)
-                {
-                        const Vertex& v1 = vertices[it->v1];
-                        const Vertex& v2 = vertices[it->v2];
-                        const Vertex& v3 = vertices[it->v3];
-                        glVertex3fv(v1.position.elems);
-                        glVertex3fv(v2.position.elems);
-                        glVertex3fv(v2.position.elems);
-                        glVertex3fv(v3.position.elems);
-                        glVertex3fv(v3.position.elems);                                                                                                
-                        glVertex3fv(v1.position.elems);                                                                                                                     
-                }
-        glEnd();
-}
-
 void Surface::render()
-{	
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glBegin(GL_POINTS);
-	for (VertexList::const_iterator v = verts.begin(); (v != verts.end()); v++)
-		glVertex3fv(v->position.elems);
-	glEnd();
-	
+{		
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glBegin(GL_TRIANGLES);
 	for (TriangleList::const_iterator it = tris.begin(); (it != tris.end()); it++)
 		renderTriangle(*it);
     glEnd();
-    
-    glColor3f(0.0f, 0.0f, 1.0f);
-    renderLines(verts, tris);
 }
 
 void Surface::renderVertex(const Vertex& v)
