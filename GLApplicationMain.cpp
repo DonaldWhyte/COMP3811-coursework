2c29489d42ef61f5419fcf48657babfa625b8ee8
Donald Whyte <sc10dw@cslin071.csunix.comp.leeds.ac.uk>
2013-10-31 19:04:28 +0000

    Constructed person using just bones and surface factory objects

--- GLApplicationMain.cpp
+++ GLApplicationMain.cpp
@@ -9,7 +9,7 @@
 
 Bone* createLeg(SurfaceFactory* surfaceFactory, Vector3 rootPosition, Vector3 rootOrientation)
 {
-    Surface* joint = surfaceFactory->createSphere(0.4f, 32, 32);    
+    Surface* joint = surfaceFactory->createSphere(0.4f, 32, 32);
     Surface* legComponent = surfaceFactory->createCylinder(1.6f, 0.3f, 32);
     Surface* foot = surfaceFactory->createBox(0.7f, 1.1f, 0.5f);
     Bone* thigh = new Bone(legComponent, rootPosition, rootOrientation);
@@ -21,6 +21,35 @@ Bone* createLeg(SurfaceFactory* surfaceFactory, Vector3 rootPosition, Vector3 ro
     shin->addChild(leg);
     
     return thigh;
+}
+
+Bone* createArm(SurfaceFactory* surfaceFactory, Vector3 rootPosition, Vector3 rootOrientation, bool reverseDirection)
+{
+    float armDirection = 10.0f;
+    Vector3 upperArmPosition(0.15f, -0.7f, 0.0f);    
+    Vector3 lowerArmPosition(-0.14f, 0.0f, 0.8f);     
+    if (reverseDirection)
+    {
+        armDirection = -armDirection;
+        upperArmPosition.x = -upperArmPosition.x;
+        lowerArmPosition.x = -lowerArmPosition.x;
+    }
+   
+    Surface* shoulderJoint = surfaceFactory->createSphere(0.4f, 32, 32);
+    Surface* upperArmComponent = surfaceFactory->createCylinder(2.0f, 0.25f, 32);
+    Surface* elbowJoint = surfaceFactory->createSphere(0.3f, 32, 32);
+    Surface* lowerArmComponent = surfaceFactory->createCylinder(1.4f, 0.25f, 32);    
+        
+    Bone* shoulder = new Bone(shoulderJoint, rootPosition, rootOrientation);
+    Bone* upperArm = new Bone(upperArmComponent, upperArmPosition, Vector3(90.0f, armDirection, 0.0f));
+    shoulder->addChild(upperArm);
+    Bone* elbow = new Bone(elbowJoint, Vector3(0.0f, 0.0f, 1.1f), Vector3(0, 0, 0));
+    upperArm->addChild(elbow);    
+    Bone* lowerArm = new Bone(lowerArmComponent, lowerArmPosition, Vector3(0, -armDirection, 0));
+    elbow->addChild(lowerArm);
+    Bone* hand = new Bone(elbowJoint, Vector3(0, 0, 0.9f), Vector3(0, 0, 0));
+    lowerArm->addChild(hand);
+    return shoulder;
 }
 
 int main(int argc, char* argv[])
@@ -48,7 +77,12 @@ int main(int argc, char* argv[])
         Vector3(0.0f, 0.0f, -0.7f));
     neckBone->addChild(headBone);
     // Create person's left and right arms
-    // TODO
+    Bone* leftArm = createArm(&surfaceFactory,
+        Vector3(-0.2f, 1.9f, 0.55f), Vector3(0.0f, 0.0f, 0.0f), true);
+    Bone* rightArm = createArm(&surfaceFactory,
+        Vector3(2.2f, 1.9f, 0.55f), Vector3(0.0f, 0.0f, 0.0f), false);
+    chestBone->addChild(leftArm);        
+    chestBone->addChild(rightArm);
     // Create person's left and right legs
     Bone* leftLeg = createLeg(&surfaceFactory,
         Vector3(0.5f, -0.8f, 0.55f), Vector3(90.0f, 0.0f, 0.0f));
@@ -63,7 +97,7 @@ int main(int argc, char* argv[])
 	GLWindow* window = new GLWindow(NULL, compositeDrawable);
 	GLApplicationController* controller = new GLApplicationController(window, compositeDrawable);
 
-	window->resize(640, 640);
+	window->resize(1280, 1280);
 	window->show();
 
 	app.exec();
